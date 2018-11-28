27d08f53eedae435e9aadea9c304e36ffce7d94d
NightKing0919 <narugoku94@gmail.com>
2018-11-28 12:15:19 +0530

    Initial Update

---
+++ src/Arrays/kadaneAlgorithm.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class kadaneAlgorithm {
+
+    public static int maxSumInArray(int[] arr) {
+        //Kadane's Algorithm
+        int max = 0;
+        int temp = 0;
+
+        for (int x : arr) {
+            temp = temp + x;
+            temp = Math.max(temp, 0);
+            max = Math.max(temp, max);
+        }
+        return max;
+    }
+}

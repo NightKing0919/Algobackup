6e41684b93dc0347585b13ffd96a759f8f693342
SandViper <svamsiiscrazy@gmail.com>
2019-03-14 00:33:23 +0530

    Trees-->3 traversals
    Signed-off-by: SandViper <svamsiiscrazy@gmail.com>

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -3,24 +3,48 @@ package Trees;
 public class BinaryTree {
 
     Node root;
-    public BinaryTree(int key){
-        root=new Node(key);
-    }
-    public BinaryTree(){
-        root=null;
-    }
-    public void setRoot(int key){
-        root.key=key;
+
+    public BinaryTree(Node root) {
+        this.root = root;
     }
 
-}
+    public BinaryTree() {
+        root = null;
+    }
 
-class Node{
-    int key;
-    Node left,right;
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
 
-    public Node(int item){
-        key=item;
-        left=right=null;
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        printInorder(tree.root);
+        System.out.println();
+        printPreOrder(tree.root);
+        System.out.println();
+        printPostOrder(tree.root);
+    }
+
+    static void printInorder(Node node) {
+        if (node == null) return;
+        printInorder(node.left);
+        System.out.print(node.data + " ");
+        printInorder(node.right);
+    }
+
+    static void printPreOrder(Node node) {
+        if (node == null) return;
+        System.out.print(node.data + " ");
+        printPreOrder(node.left);
+        printPreOrder(node.right);
+    }
+
+    static void printPostOrder(Node node) {
+        if (node == null) return;
+        printPostOrder(node.left);
+        printPostOrder(node.right);
+        System.out.print(node.data + " ");
     }
 }
---
+++ src/Trees/Node.java
@@ -0,0 +1,12 @@
+package Trees;
+
+public class Node {
+
+    Node left, right;
+    int data;
+
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}

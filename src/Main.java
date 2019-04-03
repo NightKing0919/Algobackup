4e1afb819f2a6b7306dc25eff4eec03e9f661c54
NightKing0919 <narugoku94@gmail.com>
2019-04-03 15:59:12 +0530

    updated until twoSum

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,91 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] x = {-1, 0};
-        System.out.println(maxSubArray(x));
+        int[] x = {3, 3};
+        for (int xx : twoSum(x, 6)) {
+            System.out.print(xx + " ");
+        }
     }
+
+    static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) != null && map.get(target - nums[i]) != null && map.get(target - nums[i]) != i)
+                return new int[]{i, map.get(target - nums[i])};
+        }
+        return new int[]{};
+    }
+
+    //Generates pascal triangle upto n levels.
+    static List<List<Integer>> generatePascalTriange(int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (n == 0) {
+            return res;
+        }
+        List<Integer> temp = new ArrayList<>();
+        temp.add(1);
+        res.add(temp);
+        for (int i = 2; i <= n; i++) {
+            try {
+                temp = res.get(i - 2);
+            } catch (Exception e) {
+                System.out.println("i = " + i);
+            }
+            List<Integer> x = new ArrayList<>();
+            x.add(1);
+
+            for (int j = 0; j < temp.size() - 1; j++) {
+                x.add(temp.get(j) + temp.get(j + 1));
+            }
+            x.add(1);
+            res.add(x);
+        }
+        return res;
+    }
+
+    static void SetMatrix(int[][] matrix) {
+        HashSet<Integer> setrow = new HashSet<>();
+        HashSet<Integer> setcol = new HashSet<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int ele = matrix[i][j];
+                if (ele == 0) {
+                    setrow.add(i);
+                    setcol.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (setrow.contains(i) || setcol.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
 
     static int maxSubArray(int[] nums) {
         int curmax = nums[0];
         int max = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            if(nums[i]<=0){
-                curmax=nums[i];
-                max=Math.max(curmax,max);
+            if (nums[i] <= 0) {
+                curmax = nums[i];
+                max = Math.max(curmax, max);
+            } else {
+                curmax += nums[i];
+                max = Math.max(curmax, max);
             }
-            else {
-                curmax+=nums[i];
-                max=Math.max(curmax,max);
-            }
-        }return max;
+        }
+        return max;
     }
 
     //  [7,8,9,0,1,2,3,4,5,6]

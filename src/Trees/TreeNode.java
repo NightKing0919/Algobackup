f9c17366862c89b79c61dcce7edcb5f66e4d3853
NightKing0919 <narugoku94@gmail.com>
2019-04-16 16:37:28 +0530

    Completed AddTwoNumbers-Linked List Leetcode.com

---
+++ src/Trees/TreeNode.java
@@ -0,0 +1,12 @@
+package Trees;
+
+public class TreeNode    {
+
+    TreeNode left, right;
+    int val;
+
+    public TreeNode(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
